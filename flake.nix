96d2ac1db5b487e938199bcb03e034622afc5ff0
carter <cartercoding88@gmail.com>
2026-07-07 15:06:09 -0500

    flake works finally???

--- flake.nix
+++ flake.nix
@@ -4,78 +4,72 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     crane.url = "github:ipetkov/crane";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      crane,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = {self, nixpkgs, crane}:
+  let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+  in {
+    packages = forAllSystems (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = pkgsFor.${system};
         craneLib = crane.mkLib pkgs;
-
         commonArgs = {
           src = craneLib.cleanCargoSource ./.;
-          strictDeps = true; # not sure what this is but seems important
-          nativeBuildInputs = [pkgs.pkg-config];
-          buildInputs = [pkgs.libinput];
+          strictDeps = true;
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.libinput ];
         };
-
-        better-touchbar = craneLib.buildPackage (
+      in {
+        default = craneLib.buildPackage (
           commonArgs
           // {
             cargoArtifacts = craneLib.buildDepsOnly commonArgs;
           }
         );
-      in
-      {
-        checks = {
-          inherit better-touchbar;
-        };
-
-        packages.default = better-touchbar;
-
-        nixosModules.default = {config, pkgs, lib, ...}: let
-          inherit (lib) mkEnableOption mkIf mkOption;
-          inherit (lib.types) package;
-          config' = config.services.better-touchbar;
-        in {
-          options = {
-            services.better-touchbar = {
-              enable = mkEnableOption "whether to enable the better-touchbar service";
-              package = mkOption {
-                type = package;
-                default = self.packages.${pkgs.system}.default;
-                description = "package for better-touchbar";
-              };
-            };
-          };
-          
-          config = mkIf config'.enable {
-            systemd.services."better-touchbar" = {
-              enable = true;
-              description = "pretty touchbar";
-              unitConfig = {
-                Type = "simple";
-              };
-              
-              serviceConfig = {
-                ExecStart = "${config'.package}/bin/better-touchbar";
-              };
-
-              after = ["systemd-user-sessions.service" "getty@tty1.service" "plymouth-quit.service" "systemd-logind.service"];
-            };
-          };
-        };
-
-        devShells.default = craneLib.devShell {};
       }
     );
+
+    devShells = forAllSystems (system:
+      let
+        pkgs = pkgsFor.${system};
+        craneLib = crane.mkLib pkgs;
+      in
+        craneLib.devShell {}
+    );
+
+    nixosModules.better-touchbar = {config, pkgs, lib, ...}: let
+      inherit (lib) mkEnableOption mkIf mkOption;
+      inherit (lib.types) package;
+      config' = config.services.better-touchbar;
+    in {
+      options = {
+        services.better-touchbar = {
+          enable = mkEnableOption "whether to enable the better-touchbar service";
+          package = mkOption {
+            type = package;
+            default = self.packages.${pkgs.system}.default;
+            description = "package for better-touchbar";
+          };
+        };
+      };
+      
+      config = mkIf config'.enable {
+        systemd.services."better-touchbar" = {
+          enable = true;
+          description = "pretty touchbar";
+          
+          serviceConfig = {
+            ExecStart = "${config'.package}/bin/better-touchbar";
+            Restart = "always";
+          };
+
+          after = ["systemd-user-sessions.service" "getty@tty1.service" "plymouth-quit.service" "systemd-logind.service"];
+          wantedBy = ["multi-user.target"];
+        };
+      };
+    };
+  };
 }
