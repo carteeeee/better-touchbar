5e537bd8e6b05d06520fd670fd9c0d26f3bc3502
carter <cartercoding88@gmail.com>
2026-07-07 14:17:04 -0500

    make it actually actually work?

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "apple touchbar on nix real";
+  description = "service to show pretty colors on the apple touchbar";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -9,6 +9,7 @@
 
   outputs =
     {
+      self,
       nixpkgs,
       crane,
       flake-utils,
@@ -40,6 +41,39 @@
         };
 
         packages.default = better-touchbar;
+
+        nixosModules.default = {config, pkgs, lib, ...}: let
+          inherit (lib) mkEnableOption mkIf mkOption;
+          inherit (lib.types) package;
+          config' = config.services.better-touchbar;
+        in {
+          options = {
+            services.better-touchbar = {
+              enable = mkEnableOption "whether to enable the better-touchbar service";
+              package = mkOption {
+                type = package;
+                default = self.packages.${pkgs.system}.default;
+                description = "package for better-touchbar";
+              };
+            };
+          };
+          
+          config = mkIf config'.enable {
+            systemd.services."better-touchbar" = {
+              enable = true;
+              description = "pretty touchbar";
+              unitConfig = {
+                Type = "simple";
+              };
+              
+              serviceConfig = {
+                ExecStart = "${config'.package}/bin/better-touchbar";
+              };
+
+              after = ["systemd-user-sessions.service" "getty@tty1.service" "plymouth-quit.service" "systemd-logind.service"];
+            };
+          };
+        };
 
         devShells.default = craneLib.devShell {};
       }
